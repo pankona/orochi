c0d93b74edde932fae2b21b8e104f8353042ca64
pankona <yosuke.akatsuka@gmail.com>
2019-09-23 14:05:54 +0900

    move place of main.go

---
+++ cmd/orochi/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"github.com/pankona/orochi"
+)
+
+func main() {
+	p := flag.Int("port", 3000, "specify port number of http server")
+	flag.Parse()
+
+	o := &orochi.Orochi{
+		PortList: []int{3000, 3001, 3002},
+	}
+
+	log.Fatal(o.Serve(*p))
+}
